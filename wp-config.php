bcdd86cafc82afc66bca66ead4744bc8a3d1f36b
Kevin Huang <kevinhuangdreamer@gmail.com>
2015-05-02 19:11:57 +0800

    style modification; bug fix

--- wp-config.php
+++ wp-config.php
@@ -70,7 +70,7 @@ $table_prefix  = 'wp_';
  * in their development environments.
  */
 define('WP_DEBUG', false);
-// define('WP_ENV', 'development');
+define('WP_ENV', 'development');
 
 /* That's all, stop editing! Happy blogging. */
 
--- wp-content/themes/impact/page.php
+++ wp-content/themes/impact/page.php
@@ -1,7 +1,7 @@
 <?php while (have_posts()) : the_post(); ?>
   <div id="belt"></div>
   <?php get_template_part('templates/page', 'header'); ?>
-  <div class="container" id="page-content">
+  <div id="page-content">
     <?php get_template_part('templates/content', 'page'); ?>
   </div>
 <?php endwhile; ?>
