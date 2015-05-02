da6617279e1b5764c7c44acb66e244390e46683c
Kevin Huang <kevinhuangdreamer@gmail.com>
2015-05-02 17:50:37 +0800

    fix style

--- wp-content/themes/impact/page.php
+++ wp-content/themes/impact/page.php
@@ -1,5 +1,7 @@
 <?php while (have_posts()) : the_post(); ?>
   <div id="belt"></div>
   <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+  <div class="container" id="page-content">
+    <?php get_template_part('templates/content', 'page'); ?>
+  </div>
 <?php endwhile; ?>
--- wp-content/themes/impact/templates/content-page.php
+++ wp-content/themes/impact/templates/content-page.php
@@ -1,4 +1,2 @@
-<div class="container" id="page-content">
-  <?php the_content(); ?>
-</div>
+<?php the_content(); ?>
 <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
